3153f2af7bc95b5b1d8b3bc29ebf79330be228a6
Hieu Luu <hieu.luu@niteco.se>
2018-12-19 15:39:43 +0700

    3

--- test.php
+++ test.php
@@ -1,1 +1,14 @@
-abcde. 
+<?php
+$dbhost = $_SERVER['RDS_HOSTNAME'];
+$dbport = $_SERVER['RDS_PORT'];
+$dbname = $_SERVER['RDS_DB_NAME'];
+$charset = 'utf8' ;
+
+$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
+$username = $_SERVER['RDS_USERNAME'];
+$password = $_SERVER['RDS_PASSWORD'];
+
+$pdo = new PDO($dsn, $username, $password);
+
+var_dump($pdo);
+?>
