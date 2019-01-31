edcebe9ebc5efced43bf07321639b07a6f2e1c4f
Hieu Luu <hieu.luu@niteco.se>
2019-01-31 13:39:30 +0700

    ab

--- test_camp/config.php
+++ test_camp/config.php
@@ -1,5 +1,5 @@
 <?php
 // config.php
 
-define('SMTP_PRIMARY_EMAIL', 'sender@example.com');
-define('SMTP_PRIMARY_PASSWORD', 'my very secret password');
+define('SMTP_PRIMARY_EMAIL', '6db41e4b58b67e2f096437847e13b9f6');
+define('SMTP_PRIMARY_PASSWORD', '1ccbd2e365f95859e869b9ab106f57da');
